b65fe8ac585d2aa63f4f75ac00982f5deb74f52e
unknown <ChisagVivian>
2025-07-09 18:35:07 -0500

    agrgar paquetes

---
+++ src/main/java/ec/edu/espoch/sistemaacademicopao2/personas/Persona.java
@@ -0,0 +1,26 @@
+
+package ec.edu.espoch.sistemaacademicopao2.personas;
+
+
+public class Persona {
+    
+    private int id;
+    private String Nombre;
+    private String Correopersonal;
+    private String Cedula;
+
+    public Persona(int id, String Nombre, String Correopersonal, String Cedula) {
+        this.id = id;
+        this.Nombre = Nombre;
+        this.Correopersonal = Correopersonal;
+        this.Cedula = Cedula;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "id=" + id + ", Nombre=" + Nombre + ", Correopersonal=" + Correopersonal + ", Cedula=" + Cedula + '}';
+    }
+    
+    
+    
+}
