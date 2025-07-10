7ab599aa4fd8b4df11e600c498968fa30e2819de
unknown <ChisagVivian>
2025-07-09 20:52:53 -0500

    actualizacion de clases padres

--- src/main/java/ec/edu/espoch/sistemaacademicopao2/SistemaAcademicoPAO2.java
+++ src/main/java/ec/edu/espoch/sistemaacademicopao2/SistemaAcademicoPAO2.java
@@ -2,11 +2,17 @@
 
 package ec.edu.espoch.sistemaacademicopao2;
 
+import ec.edu.espoch.sistemaacademicopao2.personas.Persona;
+
 
 public class SistemaAcademicoPAO2 {
 
     public static void main(String[] args) {
        
+        System.out.println("DATOS DEL OBJETO PERSONA");
+        Persona objpersona = new Persona(1, "Maribel Manzano", "jimi-maribel@hotmail.com", "092211");
+        
+        System.out.println("Datos " +objpersona.toString());
         
     }
 }
